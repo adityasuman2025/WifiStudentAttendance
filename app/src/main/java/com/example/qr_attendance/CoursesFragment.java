e10990a3e733743bf4efc8841dfae046a63b16d1
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-02-08 04:26:34 +0530

    list course going on

--- app/src/main/java/com/example/qr_attendance/CoursesFragment.java
+++ app/src/main/java/com/example/qr_attendance/CoursesFragment.java
@@ -3,6 +3,8 @@ package com.example.qr_attendance;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,15 +16,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -32,18 +31,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 public class CoursesFragment extends Fragment
 {
     SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    TextView text;
 
     String data[];
     ListView courseListView;
@@ -55,69 +59,96 @@ public class CoursesFragment extends Fragment
     {
         View view = inflater.inflate(R.layout.fragment_courses, null);
 
+        text = view.findViewById(R.id.text);
+        courseListView = view.findViewById(R.id.courseListView);
+
     //getting the info of the logged user
         sharedPreferences = this.getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
         String user_id_cookie = sharedPreferences.getString("user_id", "DNE");
+        editor = sharedPreferences.edit();
+
         final String user_id = decrypt(user_id_cookie);
 
-    //getting the course list of the user from database
-        courseListView = view.findViewById(R.id.courseListView);
+    //checking if phone if connected to net or not
+        ConnectivityManager connMgr = (ConnectivityManager) getActivity()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        String type = "get_user_courses";
-        try
+        if (networkInfo != null && networkInfo.isConnected()) //phone is connected
         {
-            String get_user_courses_result = (new courseData().execute(type, user_id).get());
+        //getting the course list of the user from database
+            String type = "get_user_courses";
+            try
+            {
+                String get_user_courses_result = (new courseData().execute(type, user_id).get());
 
             //parse JSON data
-            JSONArray ja = new JSONArray(get_user_courses_result);
-            JSONObject jo = null;
+                JSONArray ja = new JSONArray(get_user_courses_result);
+                JSONObject jo = null;
 
-            data = new String[ja.length()];
+                data = new String[ja.length()];
 
-            for (int i =0; i<ja.length(); i++)
-            {
-                jo = ja.getJSONObject(i);
-
-                String course_code = jo.getString("course_code");
-                String course_id = jo.getString("id");
-                data[i] = course_code + " # " + course_id;
-            }
-
-            adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, data);
-            courseListView.setAdapter(adapter);
-
-        //on clicking on any list item
-            courseListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-            {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+                String temp_courses = "";
+                for (int i =0; i<ja.length(); i++)
                 {
+                    jo = ja.getJSONObject(i);
+
+                    String course_code = jo.getString("course_code");
+                    String course_id = jo.getString("id");
+
+                    String temp = course_code + " # " + course_id;
+                    temp_courses += (temp + ",");
+
+                    data[i] = temp;
+                }
+
+            //listing courses in listview
+                adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, data);
+                courseListView.setAdapter(adapter);
+
+            //creating cookie of the registered courses of that student
+                editor.putString("studentCourses", temp_courses);
+                editor.apply();
+
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else //phone is not connected to internet
+        {
+            String studentCourses_cookie = sharedPreferences.getString("studentCourses", null);
+
+            String old_saved_courses[] = studentCourses_cookie.split(",");
+
+        //listing courses in listview
+            adapter = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, old_saved_courses);
+            courseListView.setAdapter(adapter);
+        }
+
+    //on clicking on any list item
+        courseListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
                 //creating cookie for course_id
-                    String listViewText = ((TextView)view).getText().toString();
-                    String temp[] = listViewText.split(" # ");
+                String listViewText = ((TextView)view).getText().toString();
+                String temp[] = listViewText.split(" # ");
 
-                    String course_id_cookie = temp[ temp.length - 1];
+                String course_id_cookie = temp[ temp.length - 1];
 
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString("course_id", course_id_cookie);
-                    editor.apply();
+                editor.putString("course_id", course_id_cookie);
+                editor.apply();
 
                 //redirecting to the qr code generator page
-                    Intent QRCodeGeneratorIntent = new Intent(getActivity().getApplicationContext(), AttendanceQR.class);
-                    startActivity(QRCodeGeneratorIntent);
-                    //finish(); //used to delete the last activity history which we don't want to delete
-
-                    //Toast.makeText(getActivity().getApplicationContext(), course_id_cookie, Toast.LENGTH_SHORT).show();
-                }
-            });
-
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+                Intent QRCodeGeneratorIntent = new Intent(getActivity().getApplicationContext(), AttendanceQR.class);
+                startActivity(QRCodeGeneratorIntent);
+            }
+        });
 
         return view;
     }
@@ -132,28 +163,6 @@ public class CoursesFragment extends Fragment
     public static String decrypt(String input)
     {
         return new String(Base64.decode(input, Base64.DEFAULT));
-    }
-}
-
-class Courses
-{
-    private String course_code;
-    private String course_id;
-
-    public Courses(String course_code, String course_id)
-    {
-        this.course_code = course_code;
-        this.course_id = course_id;
-    }
-
-    public String getCourse_code()
-    {
-        return course_code;
-    }
-
-    public String getCourse_id()
-    {
-        return course_id;
     }
 }
 
