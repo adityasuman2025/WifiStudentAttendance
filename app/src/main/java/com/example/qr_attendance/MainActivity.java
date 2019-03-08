f2fe0112e09d3220548ba91241c04e4cb93a82a7
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-03-08 18:49:44 +0530

    http request issue on android 9 solved

--- app/src/main/java/com/example/qr_attendance/MainActivity.java
+++ app/src/main/java/com/example/qr_attendance/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,9 +30,15 @@ public class MainActivity extends AppCompatActivity {
     TextView login_feed;
 
     SharedPreferences sharedPreferences;
+
     String androidId;
     String uniqueID;
 
+    int androidVersion;
+
+    int PERMISSION_CODE = 1;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,68 +68,92 @@ public class MainActivity extends AppCompatActivity {
         //to get unique identification of a phone and displaying it
         androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID); // android id
 
-        uniqueID = Build.SERIAL;
-
-    //on clicking on login button
-        login_btn.setOnClickListener(new View.OnClickListener()
+        // Permission is not granted of READ_PHONE_STATE
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
         {
-            @Override
-            public void onClick(View view)
+            //asking for grating the permission
+            login_btn.setOnClickListener(new View.OnClickListener()
             {
-                String roll_no = roll_no_input.getText().toString().toUpperCase();
-                String password = password_input.getText().toString();
-                String type = "verify_login";
-
-            //checking if phone if connected to net or not
-                ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-                if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED)
+                @Override
+                public void onClick(View view)
                 {
-                //trying to login the user
-                    try
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_CODE);
+                }
+            });
+        }
+        // Permission is granted
+        else
+        {
+            androidVersion = android.os.Build.VERSION.SDK_INT;
+
+            if (androidVersion < 28) //less than 9 (SDK: 29)
+            {
+                uniqueID = android.os.Build.SERIAL; // Serial_no
+            } else {
+                uniqueID = Build.getSerial();
+            }
+
+        //on clicking on login button
+            login_btn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    String roll_no = roll_no_input.getText().toString().toUpperCase();
+                    String password = password_input.getText().toString();
+                    String type = "verify_login";
+
+                    //checking if phone if connected to net or not
+                    ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                    if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                            connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED)
                     {
-                        String login_result = new DatabaseActions().execute(type, roll_no, password, androidId, uniqueID).get();
-
-                        if(login_result.equals("-1"))
+                        //trying to login the user
+                        try
                         {
-                            login_feed.setText("Database issue found");
-                        }
-                        else if(login_result.equals("Something went wrong"))
-                        {
-                            login_feed.setText(login_result);
-                        }
-                        else if(Integer.parseInt(login_result)> 0)
-                        {
-                            //creating cookie of the logged in user
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putString("roll_no", new Encryption().encrypt(roll_no));
-                            editor.putString("user_id", new Encryption().encrypt(login_result));
-                            editor.apply();
+                            String login_result = new DatabaseActions().execute(type, roll_no, password, androidId, uniqueID).get();
 
-                            //login_feed.setText(Integer.toString(login_result));
+                            if(login_result.equals("-1"))
+                            {
+                                login_feed.setText("Database issue found");
+                            }
+                            else if(login_result.equals("Something went wrong"))
+                            {
+                                login_feed.setText(login_result);
+                            }
+                            else if(Integer.parseInt(login_result)> 0)
+                            {
+                                //creating cookie of the logged in user
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("roll_no", new Encryption().encrypt(roll_no));
+                                editor.putString("user_id", new Encryption().encrypt(login_result));
+                                editor.apply();
 
-                            //redirecting the list course page
-                            Intent ListCourseIntent = new Intent(MainActivity.this, Dashboard.class);
-                            startActivity(ListCourseIntent);
-                            finish(); //used to delete the last activity history which we don't want to delete
-                        }
-                        else
-                        {
-                            login_feed.setText("Your login credentials may be incorrect or this may be not your registered phone.");
-                        }
+                                //login_feed.setText(Integer.toString(login_result));
 
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                                //redirecting the list course page
+                                Intent ListCourseIntent = new Intent(MainActivity.this, Dashboard.class);
+                                startActivity(ListCourseIntent);
+                                finish(); //used to delete the last activity history which we don't want to delete
+                            }
+                            else
+                            {
+                                login_feed.setText("Your login credentials may be incorrect or this may be not your registered phone.");
+                            }
+
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else
+                    {
+                        login_feed.setText("Internet connection is not available");
                     }
                 }
-                else
-                {
-                    login_feed.setText("Internet connection is not available");
-                }
-            }
-        });
+            });
+        }
 
     //on clicking on register button
         register_btn.setOnClickListener(new View.OnClickListener()
--- app/src/main/java/com/example/qr_attendance/Register.java
+++ app/src/main/java/com/example/qr_attendance/Register.java
@@ -1,23 +1,30 @@
 package com.example.qr_attendance;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.concurrent.ExecutionException;
 
-public class Register extends AppCompatActivity
-{
-//defining variables
+public class Register extends AppCompatActivity {
+    //defining variables
     EditText reg_name_input;
     EditText reg_roll_input;
     EditText reg_pass_input;
@@ -30,6 +37,11 @@ public class Register extends AppCompatActivity
     String androidId;
     String uniqueID;
 
+    int androidVersion;
+
+    int PERMISSION_CODE = 1;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -44,110 +56,160 @@ public class Register extends AppCompatActivity
         reg_feed = findViewById(R.id.reg_feed);
         reg_new_btn = findViewById(R.id.reg_new_btn);
 
-    //to get unique identification of a phone and displaying it
+        //to get unique identification of a phone and displaying it
         androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID); // android id
-        uniqueID = android.os.Build.SERIAL; // Serial_no
 
-        reg_new_btn.setOnClickListener(new View.OnClickListener()
+        // Permission is not granted of READ_PHONE_STATE
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
         {
-            @Override
-            public void onClick(View view) {
-                String reg_name = reg_name_input.getText().toString();
-                String reg_roll = reg_roll_input.getText().toString();
-                String reg_pass = reg_pass_input.getText().toString();
-                String reg_con_pass = reg_con_pass_input.getText().toString();
+            //asking for grating the permission
+            reg_new_btn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    ActivityCompat.requestPermissions(Register.this, new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_CODE);
+                }
+            });
+        }
+        // Permission is granted
+        else
+        {
+            androidVersion = android.os.Build.VERSION.SDK_INT;
 
-                //checking if phone if connected to net or not
-                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-                if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-                    if (reg_pass.equals(reg_con_pass) && reg_pass.length() != 0 && reg_con_pass.length() != 0)
-                    {
-                    //check if that phone is already registered or not
-                        String type = "check_phone_registered";
-                        try {
-                            String check_phone_result = new DatabaseActions().execute(type, androidId, uniqueID).get();
+            if (androidVersion < 28) //less than 9 (SDK: 29)
+            {
+                uniqueID = android.os.Build.SERIAL; // Serial_no
+            }
+            else
+            {
+                uniqueID = Build.getSerial();
+            }
 
-                            if (check_phone_result.equals("1"))//yes phone is already registered
-                            {
-                                reg_feed.setText("This phone is already registered for a student.");
-                            }
-                            else if (check_phone_result.equals("-1"))
-                            {
-                                reg_feed.setText("Database issue found");
-                            }
-                            else if (check_phone_result.equals("Something went wrong"))
-                            {
-                                reg_feed.setText(check_phone_result);
-                            }
-                            else
-                            {
-                                //checking if all input fields have been filled or not
-                                if (reg_name.length() != 0 && reg_roll.length() != 0)
+        //on clicking on register button
+            reg_new_btn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view) {
+                    String reg_name = reg_name_input.getText().toString();
+                    String reg_roll = reg_roll_input.getText().toString();
+                    String reg_pass = reg_pass_input.getText().toString();
+                    String reg_con_pass = reg_con_pass_input.getText().toString();
+
+                    //checking if phone if connected to net or not
+                    ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                    if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                            connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+                        if (reg_pass.equals(reg_con_pass) && reg_pass.length() != 0 && reg_con_pass.length() != 0)
+                        {
+                            //check if that phone is already registered or not
+                            String type = "check_phone_registered";
+                            try {
+                                String check_phone_result = new DatabaseActions().execute(type, androidId, uniqueID).get();
+
+                                if (check_phone_result.equals("1"))//yes phone is already registered
                                 {
-                                    //check if that roll number is already registered or not
-                                    type = "check_roll_exist";
-                                    String check_roll_result = new DatabaseActions().execute(type, reg_roll).get();
+                                    reg_feed.setText("This phone is already registered for a student.");
+                                }
+                                else if (check_phone_result.equals("-1"))
+                                {
+                                    reg_feed.setText("Database issue found");
+                                }
+                                else if (check_phone_result.equals("Something went wrong"))
+                                {
+                                    reg_feed.setText(check_phone_result);
+                                }
+                                else
+                                {
+                                    //checking if all input fields have been filled or not
+                                    if (reg_name.length() != 0 && reg_roll.length() != 0)
+                                    {
+                                        //check if that roll number is already registered or not
+                                        type = "check_roll_exist";
+                                        String check_roll_result = new DatabaseActions().execute(type, reg_roll).get();
 
-                                    if(check_roll_result.equals("1"))//yes roll number already exist
-                                    {
-                                        reg_feed.setText("This Roll Number is already registered.");
-                                    } else if (check_roll_result.equals("-1")) {
-                                        reg_feed.setText("Database issue found");
-                                    }
-                                    else if (check_roll_result.equals("Something went wrong"))
-                                    {
-                                        reg_feed.setText(check_roll_result);
-                                    }
-                                    else
-                                    {
-                                        //if everything fine then registering the new user in the databse
-                                        type = "register_new_user_in_db";
-                                        String register_new_user_result = new DatabaseActions().execute(type, reg_roll, reg_name, reg_pass, androidId, uniqueID).get();
-
-                                        if (register_new_user_result.equals("-1"))
+                                        if(check_roll_result.equals("1"))//yes roll number already exist
                                         {
+                                            reg_feed.setText("This Roll Number is already registered.");
+                                        } else if (check_roll_result.equals("-1")) {
                                             reg_feed.setText("Database issue found");
                                         }
-                                        else if(register_new_user_result.equals("Something went wrong"))
+                                        else if (check_roll_result.equals("Something went wrong"))
                                         {
-                                            reg_feed.setText(register_new_user_result);
+                                            reg_feed.setText(check_roll_result);
                                         }
-                                        else if (Integer.parseInt(register_new_user_result)> 0)//successfully registered
+                                        else
                                         {
-                                            //creating cookie of the logged in user
-                                            sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
-                                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                                            editor.putString("roll_no", new Encryption().encrypt(reg_roll));
-                                            editor.putString("user_id", new Encryption().encrypt(register_new_user_result));
-                                            editor.apply();
+                                            //if everything fine then registering the new user in the databse
+                                            type = "register_new_user_in_db";
+                                            String register_new_user_result = new DatabaseActions().execute(type, reg_roll, reg_name, reg_pass, androidId, uniqueID).get();
 
-                                            //reg_feed.setText("Successfully registered");
+                                            if (register_new_user_result.equals("-1"))
+                                            {
+                                                reg_feed.setText("Database issue found");
+                                            }
+                                            else if(register_new_user_result.equals("Something went wrong"))
+                                            {
+                                                reg_feed.setText(register_new_user_result);
+                                            }
+                                            else if (Integer.parseInt(register_new_user_result)> 0)//successfully registered
+                                            {
+                                                //creating cookie of the logged in user
+                                                sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+                                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                                editor.putString("roll_no", new Encryption().encrypt(reg_roll));
+                                                editor.putString("user_id", new Encryption().encrypt(register_new_user_result));
+                                                editor.apply();
 
-                                            //redirecting the list course page
-                                            Intent ListCourseIntent = new Intent(Register.this, Dashboard.class);
-                                            startActivity(ListCourseIntent);
-                                            finish(); //used to delete the last activity history which we want to delete
-                                        } else {
-                                            reg_feed.setText("Something went wrong registering user");
+                                                //reg_feed.setText("Successfully registered");
+
+                                                //redirecting the list course page
+                                                Intent ListCourseIntent = new Intent(Register.this, Dashboard.class);
+                                                startActivity(ListCourseIntent);
+                                                finish(); //used to delete the last activity history which we want to delete
+                                            } else {
+                                                reg_feed.setText("Something went wrong registering user");
+                                            }
                                         }
+                                    } else {
+                                        reg_feed.setText("Please fill all the input fields");
                                     }
-                                } else {
-                                    reg_feed.setText("Please fill all the input fields");
                                 }
+                            } catch (ExecutionException e) {
+                                e.printStackTrace();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
                             }
-                        } catch (ExecutionException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                        } else {
+                            reg_feed.setText("Password do not match");
                         }
                     } else {
-                        reg_feed.setText("Password do not match");
+                        reg_feed.setText("Internet connection is not available");
                     }
-                } else {
-                    reg_feed.setText("Internet connection is not available");
                 }
-            }
-        });
+            });
+        }
     }
+
+//function to ask for permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        if(PERMISSION_CODE == 1)//READ_PHONE_STATE
+        {
+        //restarting app
+            finish();
+            startActivity(getIntent());
+
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "Permission Granted! Please Restart the App.", Toast.LENGTH_SHORT);
+            }
+            else
+            {
+                Toast.makeText(this, "Permission Not Granted", Toast.LENGTH_SHORT);
+            }
+        }
+    }
+
 }
