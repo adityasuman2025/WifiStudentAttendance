5458b582b37ce84b2361aa9d529de4803d7adb7e
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-04-07 12:15:28 +0530

    setting up layout

--- app/src/main/java/com/example/studentattendance/AttendanceQR.java
+++ app/src/main/java/com/example/studentattendance/AttendanceQR.java
@@ -3,9 +3,14 @@ package com.example.studentattendance;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,18 +22,27 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import org.json.JSONArray;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class AttendanceQR extends AppCompatActivity
 {
-    SharedPreferences sharedPreferences;
-    TextView qr_text;
-    ImageView qr_view;
-
     TextView courseCode;
-    TextView clock;
+
+    ImageView wifImg;
+    Button markBtn;
+    TextView qr_text;
+
+    String hotspotIP = "192.168.43.1";
+    int port = 3399;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,62 +50,165 @@ public class AttendanceQR extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_qr);
 
-        qr_text = findViewById(R.id.qr_text);
-        qr_view = findViewById(R.id.qr_view);
         courseCode = findViewById(R.id.courseCode);
 
-    //to show running clock
-        clock = findViewById(R.id.clock);
-
-        final Handler someHandler = new Handler(getMainLooper());
-        someHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                clock.setText(new SimpleDateFormat("hh:mm:ss a").format(new Date()));
-                someHandler.postDelayed(this, 1000);
-            }
-        }, 10);
+        wifImg = findViewById(R.id.wifImg);
+        markBtn = findViewById(R.id.markBtn);
+        qr_text = findViewById(R.id.qr_text);
 
     //to get the cookie values
         sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
-        String user_id_cookie = new Encryption().decrypt(sharedPreferences.getString("user_id", "DNE"));
+        final String user_id_cookie = new Encryption().decrypt(sharedPreferences.getString("user_id", "DNE"));
 
         String course_code_cookie = sharedPreferences.getString("course_code", "DNE");
-        String course_id_cookie = sharedPreferences.getString("course_id", "DNE");
+        final String course_id_cookie = sharedPreferences.getString("course_id", "DNE");
 
         courseCode.setText(course_code_cookie);
 
-    //to get current timestamps
-        Long tsLong = System.currentTimeMillis()/1000;
-        String ts = tsLong.toString();
+    //checking if wifi is on or not
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
-    //storing info into JSON and encrypting it
-        String qr_data[] = {user_id_cookie, course_id_cookie, ts}; //JSON format: userID, courseID, currentTimestamps
-        JSONArray mJSONArray = new JSONArray(Arrays.asList(qr_data));
-
-        String encrypted_data = new Encryption().encrypt(mJSONArray.toString());
-
-        if(user_id_cookie != null && course_id_cookie != null)
+        if(!mWifi.isConnected()) //if wifi is not connected
         {
+            qr_text.setText("Your mobile Wifi is OFF. Turn it ON and restart the App");
+        }
+        else//if wifi is connected
+        {
+            wifImg.setImageResource(R.drawable.wifi);
+        }
+
+    //on clicking on mark attendance button
+        markBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+            //checking if wifi is on or not
+                ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+                if(!mWifi.isConnected()) //if wifi is not connected
+                {
+                    qr_text.setText("Your mobile Wifi is OFF. Turn it ON and restart the App");
+                }
+                else//if wifi is connected
+                {
+                //on clicking on mark button
+                    markBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view)
+                        {
+                            //to get current timestamps
+                            Long tsLong = System.currentTimeMillis()/1000;
+                            String ts = tsLong.toString();
+
+                            //storing attendance info into JSON and encrypting it
+                            String qr_data[] = {user_id_cookie, course_id_cookie, ts}; //JSON format: userID, courseID, currentTimestamps
+                            JSONArray mJSONArray = new JSONArray(Arrays.asList(qr_data));
+
+                            String encrypted_data = new Encryption().encrypt(mJSONArray.toString());
+
+                            if(user_id_cookie != null && course_id_cookie != null)
+                            {
+                                MyClientTask myClientTask = new MyClientTask(hotspotIP, port, encrypted_data);
+                                myClientTask.execute();
+                            }
+                            else
+                            {
+                                qr_text.setText("Something went wrong");
+                            }
+                        }
+                    });
+                }
+            }
+       });
+    }
+
+//inner class for using socket in wifi
+    public class MyClientTask extends AsyncTask<Void, Void, Void>
+    {
+        String dstAddress;
+        int dstPort;
+        String msgToServer;
+
+        String response = "";
+
+        MyClientTask(String addr, int port, String msgTo)
+        {
+            dstAddress = addr;
+            dstPort = port;
+            msgToServer = msgTo;
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0)
+        {
+            Socket socket = null;
+            DataOutputStream dataOutputStream = null;
+            DataInputStream dataInputStream = null;
+
             try
             {
-                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-                BitMatrix bitMatrix = multiFormatWriter.encode(encrypted_data, BarcodeFormat.QR_CODE,500,500);
+                socket = new Socket(dstAddress, dstPort);
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
 
-                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                if(msgToServer != null){
+                    dataOutputStream.writeUTF(msgToServer);
+                }
 
-                qr_view.setImageBitmap(bitmap);
-                //qr_text.setText(course_id_cookie);
-            }
-            catch(WriterException e)
+                response = dataInputStream.readUTF();
+
+            } catch (UnknownHostException e)
             {
                 e.printStackTrace();
+                response = "Failed to connect to the Server. You are connected to wrong WI-Fi or Host may not active at the moment";
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+                response = "Failed to connect to the Server. You are connected to wrong WI-Fi or Host may not active at the moment";;
+            } finally
+            {
+                if (socket != null)
+                {
+                    try
+                    {
+                        socket.close();
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null)
+                {
+                    try
+                    {
+                        dataOutputStream.close();
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
             }
+            return null;
         }
-        else
-        {
-            qr_text.setText("Something went wrong");
+
+        @Override
+        protected void onPostExecute(Void result) {
+            qr_text.setText(response);
+            super.onPostExecute(result);
         }
     }
+
 }
